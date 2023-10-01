987474acc4398272a06eb90680834fe1714d7e86
Juan Rojas <info.rojas@gmail.com>
2023-09-30 19:35:04 -0700

    FrmEmpleados-Funcionalidad agregada

--- ProyectoNomina/FrmEmpleados.Designer.cs
+++ ProyectoNomina/FrmEmpleados.Designer.cs
@@ -29,12 +29,251 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNumeroEmpleado = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtApellidoMaterno = new System.Windows.Forms.TextBox();
+            this.txtApellidoPaterno = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbAuxiliar = new System.Windows.Forms.RadioButton();
+            this.rbCargador = new System.Windows.Forms.RadioButton();
+            this.rbChofer = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.epError = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNumeroEmpleado);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtApellidoMaterno);
+            this.groupBox1.Controls.Add(this.txtApellidoPaterno);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 46);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(471, 255);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtNumeroEmpleado
+            // 
+            this.txtNumeroEmpleado.Location = new System.Drawing.Point(140, 15);
+            this.txtNumeroEmpleado.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNumeroEmpleado.MaxLength = 10;
+            this.txtNumeroEmpleado.Name = "txtNumeroEmpleado";
+            this.txtNumeroEmpleado.Size = new System.Drawing.Size(139, 22);
+            this.txtNumeroEmpleado.TabIndex = 0;
+            this.txtNumeroEmpleado.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumeroEmpleado_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 18);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Número Empleado:";
+            // 
+            // txtApellidoMaterno
+            // 
+            this.txtApellidoMaterno.Location = new System.Drawing.Point(140, 100);
+            this.txtApellidoMaterno.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApellidoMaterno.MaxLength = 50;
+            this.txtApellidoMaterno.Name = "txtApellidoMaterno";
+            this.txtApellidoMaterno.Size = new System.Drawing.Size(297, 22);
+            this.txtApellidoMaterno.TabIndex = 3;
+            // 
+            // txtApellidoPaterno
+            // 
+            this.txtApellidoPaterno.Location = new System.Drawing.Point(140, 71);
+            this.txtApellidoPaterno.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApellidoPaterno.MaxLength = 50;
+            this.txtApellidoPaterno.Name = "txtApellidoPaterno";
+            this.txtApellidoPaterno.Size = new System.Drawing.Size(297, 22);
+            this.txtApellidoPaterno.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 103);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Apellido Materno:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apellido Paterno:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(140, 43);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.MaxLength = 50;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(297, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbAuxiliar);
+            this.groupBox2.Controls.Add(this.rbCargador);
+            this.groupBox2.Controls.Add(this.rbChofer);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(39, 132);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(241, 97);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // rbAuxiliar
+            // 
+            this.rbAuxiliar.AutoSize = true;
+            this.rbAuxiliar.Location = new System.Drawing.Point(117, 68);
+            this.rbAuxiliar.Margin = new System.Windows.Forms.Padding(4);
+            this.rbAuxiliar.Name = "rbAuxiliar";
+            this.rbAuxiliar.Size = new System.Drawing.Size(71, 20);
+            this.rbAuxiliar.TabIndex = 2;
+            this.rbAuxiliar.TabStop = true;
+            this.rbAuxiliar.Text = "Auxiliar";
+            this.rbAuxiliar.UseVisualStyleBackColor = true;
+            // 
+            // rbCargador
+            // 
+            this.rbCargador.AutoSize = true;
+            this.rbCargador.Location = new System.Drawing.Point(117, 42);
+            this.rbCargador.Margin = new System.Windows.Forms.Padding(4);
+            this.rbCargador.Name = "rbCargador";
+            this.rbCargador.Size = new System.Drawing.Size(85, 20);
+            this.rbCargador.TabIndex = 1;
+            this.rbCargador.TabStop = true;
+            this.rbCargador.Text = "Cargador";
+            this.rbCargador.UseVisualStyleBackColor = true;
+            // 
+            // rbChofer
+            // 
+            this.rbChofer.AutoSize = true;
+            this.rbChofer.Location = new System.Drawing.Point(117, 17);
+            this.rbChofer.Margin = new System.Windows.Forms.Padding(4);
+            this.rbChofer.Name = "rbChofer";
+            this.rbChofer.Size = new System.Drawing.Size(67, 20);
+            this.rbChofer.TabIndex = 0;
+            this.rbChofer.TabStop = true;
+            this.rbChofer.Text = "Chofer";
+            this.rbChofer.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rol:";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(16, 15);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(100, 28);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(387, 308);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 28);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // epError
+            // 
+            this.epError.ContainerControl = this;
+            // 
+            // FrmEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(507, 351);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FrmEmpleados";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Empleados";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNumeroEmpleado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtApellidoMaterno;
+        private System.Windows.Forms.TextBox txtApellidoPaterno;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbAuxiliar;
+        private System.Windows.Forms.RadioButton rbCargador;
+        private System.Windows.Forms.RadioButton rbChofer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ErrorProvider epError;
     }
 }
 
--- ProyectoNomina/FrmEmpleados.cs
+++ ProyectoNomina/FrmEmpleados.cs
@@ -16,5 +16,167 @@ namespace ProyectoNomina
         {
             InitializeComponent();
         }
+
+        private void Nuevo()
+        {
+            try
+            {
+                LimpiarErrorProvider();
+                txtNombre.Text = string.Empty;
+                txtApellidoPaterno.Text = string.Empty;
+                txtApellidoMaterno.Text = string.Empty;
+                rbChofer.Checked = false;
+                rbCargador.Checked = false;
+                rbAuxiliar.Checked = false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al limpiar." + ex.Message, ex);
+            }
+        }
+        private void LimpiarErrorProvider()
+        {
+            epError.Clear();
+        }
+        private bool ValidarInformacion()
+        {
+            try
+            {
+                bool resultado = true;
+                LimpiarErrorProvider();
+
+                if (txtNumeroEmpleado.Text.Trim() == string.Empty)
+                {
+                    resultado = false;
+                    epError.SetError(txtNumeroEmpleado, "Capture un Número de Empleado por favor...");
+                }
+
+                if (txtNombre.Text.Trim() == string.Empty)
+                {
+                    resultado = false;
+                    epError.SetError(txtNombre, "Capture un Nombre por favor...");
+                }
+
+                if (txtApellidoPaterno.Text.Trim() == string.Empty)
+                {
+                    resultado = false;
+                    epError.SetError(txtApellidoPaterno, "Capture un Apellido Paterno por favor...");
+                }
+
+                if (txtApellidoMaterno.Text.Trim() == string.Empty)
+                {
+                    resultado = false;
+                    epError.SetError(txtApellidoMaterno, "Capture un Apellido Materno por favor...");
+                }
+
+                if (!(rbChofer.Checked || rbCargador.Checked || rbAuxiliar.Checked))
+                {
+                    resultado = false;
+                    epError.SetError(groupBox2, "Capture un Rol para el Empleado por favor...");
+                }
+
+                return resultado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void ValidarCapturaSoloNumeros(KeyPressEventArgs e)
+        {
+            Clipboard.Clear(); //--> es para que no copie textos.
+
+            if (!(char.IsDigit(e.KeyChar) || char.IsNumber(e.KeyChar)) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private int ObtenerCodigoRol()
+        {
+            int resultado = 0;
+            if (rbChofer.Checked)
+            {
+                resultado = 1;
+            }
+            else if (rbCargador.Checked)
+            {
+                resultado = 2;
+            }
+            else if (rbAuxiliar.Checked)
+            {
+                resultado = 3;
+            }
+            return resultado;
+        }
+
+        private void EstablecerRol(int codigoRol)
+        {
+            switch (codigoRol)
+            {
+                case 1:
+                    rbChofer.Checked = true;
+                    break;
+                case 2:
+                    rbCargador.Checked = true;
+                    break;
+                case 3:
+                    rbAuxiliar.Checked = true;
+                    break;
+            }
+        }
+
+        private void txtNumeroEmpleado_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                ValidarCapturaSoloNumeros(e);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al presionar la tecla", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Nuevo();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al limpiar el formulario", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ValidarInformacion())
+                {
+                    //Guardar o Actualizar informacion
+                    this.Cursor = Cursors.WaitCursor;
+                    
+                    /*empleadoDTO.NumeroEmpleado = int.Parse(txtNumeroEmpleado.Text.Trim());
+                    empleadoDTO.Nombre = txtNombre.Text.Trim();
+                    empleadoDTO.ApellidoPaterno = txtApellidoPaterno.Text.Trim();
+                    empleadoDTO.ApellidoMaterno = txtApellidoMaterno.Text.Trim();
+                    empleadoDTO.CodigoRol = ObtenerCodigoRol();
+                    empleadoDTO.LimiteSueldoMensual = 10000m;
+                    empleado.GuardarEmpleado(empleadoDTO);*/
+
+                    Nuevo();
+                    this.Cursor = Cursors.Default;
+                }
+            }
+            catch (Exception)
+            {
+                this.Cursor = Cursors.Default;
+                MessageBox.Show("Error al guardar la información", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
